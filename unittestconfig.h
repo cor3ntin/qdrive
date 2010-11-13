581bfd1c09f0c487caea5203722ca200f873290b
ABBAPOH <ABBAPOH@nextmail.ru>
2010-11-13 13:53:55 +0300

    Added tests

--- unittestconfig.h
+++ unittestconfig.h
@@ -1,4 +1,13 @@
 #ifndef UNITTESTCONFIG_H
 #define UNITTESTCONFIG_H
 
+#define CONSTRUCTOR "/Volumes/Macintosh HD"
+#define ROOT_PATH "/"
+#define NAME "Macintosh HD"
+#define DEVICE "/dev/disk0s2"
+#define FILESYSTEM "hfs"
+#define TYPE QDriveInfo::InternalDrive
+// next 2 values are retrieved for df utility
+#define TOTALSIZE 195050360
+#define BLOCKSIZE 512
 #endif // UNITTESTCONFIG_H
--- unittests.cpp
+++ unittests.cpp
@@ -1,1 +1,32 @@
+#include <QtTest>
 
+#include "qdriveinfo.h"
+#include "unittestconfig.h"
+
+class QDriveInfoTest: public QObject
+{
+    Q_OBJECT
+private slots:
+    void compareToConfig()
+    {
+        QDriveInfo info;
+        QVERIFY(info.isValid() == false);
+        QVERIFY(info.isReady() == false);
+        info.setRootPath(CONSTRUCTOR);
+
+        QVERIFY(info.isValid() == true);
+        QVERIFY(info.isReady() == true);
+        QVERIFY(info.rootPath() == QString::fromLocal8Bit(ROOT_PATH));
+        QVERIFY(info.name() == QString::fromLocal8Bit(NAME));
+        QVERIFY(info.device() == QString::fromLocal8Bit(DEVICE));
+        QVERIFY(info.fileSystemName() == QString::fromLocal8Bit(FILESYSTEM));
+        QVERIFY(info.type() == TYPE);
+#if defined(Q_OS_MAC)
+        QVERIFY(info.totalSize()/BLOCKSIZE == TOTALSIZE); // we devide onto block size
+#endif
+//        info.totalSize()
+    }
+};
+
+QTEST_MAIN(QDriveInfoTest)
+ #include "unittests.moc"
